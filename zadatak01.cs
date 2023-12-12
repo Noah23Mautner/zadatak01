beaff6a5958af66854ebb1b2b021ceaee3dea492
Noah23Mautner <152974783+Noah23Mautner@users.noreply.github.com>
2023-12-12 12:20:05 +0100

    Add files via upload

---
+++ zadatak01.cs
@@ -0,0 +1,21 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+int a = 10;
+int b = 3;
+int x;
+int y;
+	x=a/b;
+	y=a%b;
+		
+		
+Console.WriteLine("Dijeljenje a/b={0}", + x);
+Console.WriteLine("Ostatak={0}", +y);
+
+
+		
+	}
+}
